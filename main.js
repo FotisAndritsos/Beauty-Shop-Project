e1c81074b1a911f5140d4dc040db42b24a7b5be3
FotisAndritsos <fotisandritsos85@gmail.com>
2020-11-14 17:21:00 +0200

    slider-popular products

--- main.js
+++ main.js
@@ -49,3 +49,23 @@ const stickyNav = () =>{
     head.classList.remove("sticky");
   }
 }
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  nav:false,
+  dots:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:4
+      }
+  }
+})
